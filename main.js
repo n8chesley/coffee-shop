3a4cae96987fea9ecf0a921a6d5bc97a4e3ea2f8
nate chesley <nate@almanac.local>
2017-02-08 23:17:48 +0200

    first solution in js

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 var coffeeShop = {
   beans: 40,
-
   drinkRequirements: {
     latte: 10,
     americano: 5,
     doubleShot: 15,
     frenchPress: 12
   },
-
-  makeDrink: function (drinkType) {
-    // TODO: Finish this method
+  makeDrink: function(drinkType) {
+    //TODO: Finish this method
+    //WORKS: console.log(drinkType);
+    //WORKS: console.log(Object.keys(this.drinkRequirements));
+    //WORKS: console.log(this.drinkRequirements[(drinkType)]);
+    if (!(Object.keys(this.drinkRequirements)).includes(drinkType)) {
+      console.log("Sorry, we don't make " + drinkType + ".");
+    } else if (((Object.keys(this.drinkRequirements)).includes(drinkType)) && (this.beans >= this.drinkRequirements[(drinkType)])) {
+      this.beans -= this.drinkRequirements[(drinkType)];
+      console.log("One " + drinkType + ", coming right up!");
+    } else {
+      console.log("Sorry, we don't have enough beans left for that!");
+    }
   }
 }
 
